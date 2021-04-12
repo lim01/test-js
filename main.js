8fb54af14d0bcaaa53359ef710941d1678fd5b02
forever <lim01@naver.com>
2021-04-12 17:00:57 +0900

    Movie

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
-const user = {
-    name: 'HEROPY',
-    age: 85,
-    emails: [
-        'first@gmail.com',
-        'second@naver.com'
-    ]
+// const user = {
+//     name: 'HEROPY',
+//     age: 85,
+//     emails: [
+//         'first@gmail.com',
+//         'second@naver.com'
+//     ]
+// }
+// //localStorage.setItem('officeId', 'mclicksystem');
+// localStorage.setItem('user', JSON.stringify(user));
+// const str = localStorage.getItem('user');
+// const obj = JSON.parse(str);
+// console.log(obj);
+
+// obj.age = 22;
+
+// console.log(obj);
+
+// //localStorage.removeItem('user');
+// //localStorage.removeItem('officeId');
+
+import axios from 'axios'
+
+function fetchMovies() {
+	axios
+		.get('https://www.omdbapi.com/?apikey=7035c60c&s=frozen')
+		.then((response) => {
+			console.log(response)
+		})
 }
-//localStorage.setItem('officeId', 'mclicksystem');
-localStorage.setItem('user', JSON.stringify(user));
-const str = localStorage.getItem('user');
-const obj = JSON.parse(str);
-console.log(obj);
 
-obj.age = 22;
-
-console.log(obj);
-
-//localStorage.removeItem('user');
-//localStorage.removeItem('officeId');
+fetchMovies()
